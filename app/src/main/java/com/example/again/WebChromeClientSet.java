63659c64409b4825a17eebd833336146400ea7e4
louren1234 <louren1234@naver.com>
2020-09-01 16:26:57 +0900

    버튼 모양 바꾸는 중(ing)

--- app/src/main/java/com/example/again/WebChromeClientSet.java
+++ app/src/main/java/com/example/again/WebChromeClientSet.java
@@ -1,65 +1,84 @@
 package com.example.again;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Message;
+import android.view.ViewGroup;
 import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebChromeClientSet extends WebChromeClient {
 
-    @Override
-    public boolean onCreateWindow(WebView view, boolean dialog,
-                                  boolean userGesture, Message resultMsg) {
-        // TODO Auto-generated method stub
-        return super.onCreateWindow(view, dialog, userGesture, resultMsg);
-    }
-    @Override
-    public boolean onJsAlert(WebView view, String url, String message, final JsResult result) {
+    Dialog dialog1;
 
-        // TODO Auto-generated method stub
-        //return super.onJsAlert(view, url, message, result);
-        new AlertDialog.Builder(view.getContext())
-                .setTitle("알림")
-                .setMessage(message)
-                .setPositiveButton(android.R.string.ok,
-                        new AlertDialog.OnClickListener(){
-                            public void onClick(DialogInterface dialog, int which) {
-                                result.confirm();
-                            }
-                        })
-                .setCancelable(false)
-                .create()
-                .show();
-        return true;
+    private Activity mActivity = null;
+
+    public WebChromeClientSet(Activity activity){
+        this.mActivity = activity;
     }
 
     @Override
-    public boolean onJsConfirm(WebView view, String url, String message,
-                               final JsResult result) {
-        // TODO Auto-generated method stub
-        //return super.onJsConfirm(view, url, message, result);
-        new AlertDialog.Builder(view.getContext())
-                .setTitle("알림")
-                .setMessage(message)
-                .setPositiveButton("네",
-                        new AlertDialog.OnClickListener(){
-                            public void onClick(DialogInterface dialog, int which) {
-                                result.confirm();
-                            }
-                        })
-                .setNegativeButton("아니오",
-                        new AlertDialog.OnClickListener(){
-                            public void onClick(DialogInterface dialog, int which) {
-                                result.cancel();
-                            }
-                        })
-                .setCancelable(false)
-                .create()
-                .show();
+    public boolean onCreateWindow(WebView view, boolean isDialog, boolean isUserGesture, Message resultMsg) {
+        // Dialog Create Code
+        WebView newWebView = new WebView(mActivity);
+
+        WebSettings webSettings = newWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        dialog1 = new Dialog(mActivity);
+        dialog1.setContentView(newWebView);
+
+        ViewGroup.LayoutParams params = dialog1.getWindow().getAttributes();
+        params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+        params.height = ViewGroup.LayoutParams.MATCH_PARENT;
+        dialog1.getWindow().setAttributes((android.view.WindowManager.LayoutParams) params);
+        dialog1.show();
+        newWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onCloseWindow(WebView window) {
+
+            }
+        });
+        ((WebView.WebViewTransport) resultMsg.obj).setWebView(newWebView);
+        resultMsg.sendToTarget();
         return true;
+
+
+//        WebSettings webSettings = newWebView.getSettings();
+//        webSettings.setJavaScriptEnabled(true);
+//
+//        final Dialog dialog = new Dialog(mActivity);
+//        dialog.setContentView(newWebView);
+//
+//        ViewGroup.LayoutParams params = dialog.getWindow().getAttributes();
+//        params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+//        params.height = ViewGroup.LayoutParams.MATCH_PARENT;
+//        dialog.getWindow().setAttributes((android.view.WindowManager.LayoutParams) params);
+//        dialog.show();
+//        newWebView.setWebChromeClient(new WebChromeClient() {
+//            @Override
+//            public void onCloseWindow(WebView window) {
+//                dialog.dismiss();
+//            }
+//        });
+//
+//        // WebView Popup에서 내용이 안보이고 빈 화면만 보여 아래 코드 추가
+//        newWebView.setWebViewClient(new WebViewClient() {
+//            @Override
+//            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+////                view.loadUrl("http://52.35.235.199:3000/daum_address.php");
+//                return true;
+//            }
+//        });
+//
+//        ((WebView.WebViewTransport)resultMsg.obj).setWebView(newWebView);
+//        resultMsg.sendToTarget();
+//        return true;
+
     }
 }
